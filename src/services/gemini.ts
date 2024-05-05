ffed2788f91ffd1260307dac6bca668def3896f5
esleiter <ceo@esleiter.com>
2024-05-05 14:40:53 -0500

    Add output schema in preprompt Add Serialice output

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -2,7 +2,31 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-export const generateGemini = async (prompt: string) => {
+export const generateGemini = async (title: string, description: string) => {
+    const prompt = `
+    ${title}
+    ${description}
+    
+    Here's the output schema:
+    {
+        "client": {
+          "name": "",
+          "industry": "",
+          "location": {
+            "city": "",
+            "country": ""
+          }
+        },
+        "duration": "",
+        "budget": "",
+        "requirements": {
+          "technologies": [],
+          "requiredExperience": []
+        }
+      }
+    `;
+
     const result = await model.generateContent(prompt);
-    return result.response.text();
+    const response = JSON.parse(result.response.text());
+    return response;
 };
