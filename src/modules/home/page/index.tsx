af5a484e4837c1fc990231de330f36b7650b2149
aiston-salazar-dev <ainston.salazar@alegra.com>
2024-05-07 22:46:49 -0500

    agregado componente de respuesta de skill y detalle tecnico

--- src/modules/home/page/index.tsx
+++ src/modules/home/page/index.tsx
@@ -4,50 +4,54 @@ import { generateGemini } from "../../../services/gemini";
 import { useState } from "react";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const { handleSubmit, handleChange, values } =
-    useFormik({
-      initialValues: {
-        name: "",
-        description: "",
-      },
-      //   validationSchema: loginSchema,
-      onSubmit: async ({ name, description }) => {
-        // console.log(name, description)
-        setIsLoading(true)
-        const res = await generateGemini(name, description)
-        console.log('resp', res);
-        setIsLoading(false)
-      },
-    });
+  const [isLoading, setIsLoading] = useState(false);
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+    },
+    //   validationSchema: loginSchema,
+    onSubmit: async ({ name, description }) => {
+      // console.log(name, description)
+      setIsLoading(true);
+      const res = await generateGemini(name, description);
+      console.log("resp", res);
+      setIsLoading(false);
+    },
+  });
+
   return (
-    <Card variant="outlined" style={{ width: 750 }}>
-      <Box component="form" padding={2} noValidate onSubmit={handleSubmit}>
-        <Grid container gap={2}>
-          <TextField
-            id="name"
-            fullWidth
-            autoFocus
-            label="Nombre del proyecto"
-            value={values.name}
-            onChange={handleChange}
-          />
-          <TextField
-            id="description"
-            fullWidth
-            autoFocus
-            label="Descripcion"
-            value={values.description}
-            onChange={handleChange}
-            multiline
-            rows={4}
-          />
-        </Grid>
-        <Grid container justifyContent={'center'} marginTop={2}>
-          <Button variant="contained" type="submit" disabled={isLoading}>Enviar</Button>
-        </Grid>
-      </Box>
-    </Card>
+    <Box>
+      <Card variant="outlined" style={{ width: 750 }}>
+        <Box component="form" padding={2} noValidate onSubmit={handleSubmit}>
+          <Grid container gap={2}>
+            <TextField
+              id="name"
+              fullWidth
+              autoFocus
+              label="Nombre del proyecto"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <TextField
+              id="description"
+              fullWidth
+              autoFocus
+              label="Descripcion"
+              value={values.description}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid container justifyContent={"center"} marginTop={2}>
+            <Button variant="contained" type="submit" disabled={isLoading}>
+              Enviar
+            </Button>
+          </Grid>
+        </Box>
+      </Card>
+    </Box>
   );
 };
 
