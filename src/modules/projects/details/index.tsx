6a650be36f5ab1971927b7f170a951f711002a69
esleiter <ceo@esleiter.com>
2024-05-11 07:11:56 -0500

    Fix Types

--- src/modules/projects/details/index.tsx
+++ src/modules/projects/details/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { useGetById } from "../../../services/useSaveData";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -11,27 +11,6 @@ export type Match = {
   score: string;
   justifyResume: string;
 };
-
-const respmatch = [
-  {
-    consultantId: "3eEJj7zrlIOz4SPznuDP",
-    score: "70%",
-    justifyResume:
-      "El consultor tiene experiencia en proyectos similares, dentro del mismo rango de duración y presupuesto. Además, su CV muestra habilidades relevantes para el alcance del proyecto.",
-  },
-  {
-    consultantId: "SgutFheiJIsN6BbiiXjW",
-    score: "60%",
-    justifyResume:
-      "El consultor tiene experiencia en proyectos similares, pero de menor duración y presupuesto. Sin embargo, su CV muestra habilidades relevantes para el alcance del proyecto.",
-  },
-  {
-    consultantId: "fN7qzGDub4Wrf1K7uiyr",
-    score: "50%",
-    justifyResume:
-      "El consultor tiene experiencia en proyectos similares, pero de mayor duración y presupuesto. Además, su CV muestra algunas habilidades relevantes para el alcance del proyecto.",
-  },
-];
 
 type Params = { id: string };
 const Details = () => {
