720865212bed5f3d56980590b426fd4bba36b81f
esleiter <ceo@esleiter.com>
2024-05-10 21:18:03 -0500

    Mejora prompt

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -3,15 +3,18 @@ const genAI = new GoogleGenerativeAI(`${process.env.VITE_API_KEY}`);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 function eliminarBackticksJSON(texto: string): string {
-  // Expresión regular para encontrar ```json seguido por cualquier carácter hasta encontrar ``` o el final del texto
   const regex = /```[Jj][Ss][Oo][Nn]([\s\S]*?)```|\s*```/g;
-  // Reemplazar todas las ocurrencias de ```json seguido opcionalmente por comillas al final con una cadena vacía
   return texto.replace(regex, '');
 }
 const generateProfiles = async (title: string, description: string) => {
     const profilesPrompt = `
-    ${title}
-    ${description}
+    title :${title}
+    description: ${description}
+
+    Con el titulo y descripciones anteriores, ejecuta lo siguiente:
+
+    Do not respond with your own suggestions or recommendations or feedback.
+    Your response should strictly be a JSON object with the above schema.
     
     Here's the output schema:
     [{
@@ -34,10 +37,7 @@ const generateProfiles = async (title: string, description: string) => {
       "profileTitle": "", // e.g. "DevOps"
       "skills": [] // e.g. ["Docker", "Kubernetes", "Jenkins"]
     }]
-    IMPORTANT: ANSWER VARIOUS PROFILES ACCORDING TO THE DESCRIPTION OF THE PROJECT, ITS SCOPE, DURATION Y BUDGET, la idea es armar un equipo completo!
-
-    Do not respond with your own suggestions or recommendations or feedback.
-    Your response should strictly be a JSON object with the above schema.    
+    IMPORTANT: ANSWER VARIOUS PROFILES ACCORDING TO THE DESCRIPTION OF THE PROJECT, ITS SCOPE, DURATION Y BUDGET, la idea es armar un equipo completo!    
     `;
 
     const profiles = await model.generateContent(profilesPrompt);
@@ -48,8 +48,13 @@ const generateProfiles = async (title: string, description: string) => {
 
 export const generateGemini = async (title: string, description: string) => {
     const prompt = `
-    ${title}
-    ${description}
+    title: ${title}
+    description: ${description}
+
+    Con el titulo y descripciones anteriores, ejecuta lo siguiente:
+
+    Do not respond with your own suggestions or recommendations or feedback.
+    Your response should strictly be a JSON object with the above schema.
     
     Here's the output schema:
     
@@ -66,17 +71,14 @@ export const generateGemini = async (title: string, description: string) => {
       "budget": "", // e.g. "1000.00", "2000.00", "5000.00", "10000.00", "20000.00"
       "scope": [], // e.g. "WEB", "APP", "E-COMMERCE", "LANDING PAGE", "MOBILE", "ANDROID", "IOS"
       "requiredProfiles": []
-    }
-      
-      Do not respond with your own suggestions or recommendations or feedback.
-      Your response should strictly be a JSON object with the above schema.
-      
+    }      
       `;
 
     const result = await model.generateContent(prompt);
     const profiles = await generateProfiles(title, description);
     const eliminarBackticks = eliminarBackticksJSON(result.response.text());
     const response = JSON.parse(eliminarBackticks);
+
     response.requiredProfiles = profiles;
 
     return response;
